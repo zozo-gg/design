a0d8a20cf04a53f9059d61834a7af45699a8f646
Paliy Stepan <palstepweb@gmail.com>
2020-10-11 19:46:21 +0300

    onitoring test

--- js/main.js
+++ js/main.js
@@ -25,6 +25,52 @@ $(document).ready(function() {
 	});
 
 
+	$('.tabs__btn').on('click',function(event) {
+		var 	$this = $(this),
+				tabContainer = $this.closest('.tabs-container'),
+				tabContentItem = tabContainer.find('.tab-item'),
+				ndx = $this.index(),
+				reqItem = tabContentItem.eq(ndx);
+
+		$this.addClass('active')
+			.siblings()
+			.removeClass('active');
+
+		tabContentItem.removeClass('active');
+		reqItem.addClass('active');
+	});
+
+	if ( $(window).width() < 992) {
+		$('.server').removeClass('active');
+
+		$('.server__trigger').on('click', function(event) {
+
+			if (!$(this).closest('.server').hasClass('is_active')) {
+				$('.server').removeClass('is_active')
+				$(this).closest('.server').addClass('is_active');
+			} else {
+				$(this).closest('.server').removeClass('is_active');
+			}
+		});
+
+	}
+
+	$('.specials__trigger').on('click', function(event) {
+		$(this).siblings('.specials__list').slideToggle(350).toggleClass('active');;
+		$(this).toggleClass('active');
+	});
+
+
+	$('.top-players__title').on('click', function(event) {
+		if ( $(window).width() < 575) {
+			event.preventDefault();
+			$(this).siblings('.monitoring__players').slideToggle(350).toggleClass('active');;
+			$(this).toggleClass('active');
+		}
+	});
+
+
+
 
 	// function headerFixedToggle() {
 		
